547babab82a1e5fa4f9a8f5e498d538dff10a5b0
stepanov1997 <kristijan.stepanov95@gmail.com>
2020-04-15 22:03:08 +0200

    Updates

---
+++ precache-manifest.37d801303fc143fcd44e67db951fdd5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2349fc8ec373233831e9886617bf0ab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cf5f3245b9511f16121",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed711c850aacbfc8a5b4",
+    "url": "/static/js/2.099e8351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.099e8351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf5f3245b9511f16121",
+    "url": "/static/js/main.019d04a8.chunk.js"
+  },
+  {
+    "revision": "11a213a99cc7a9d4a820",
+    "url": "/static/js/runtime-main.1ceac2b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
